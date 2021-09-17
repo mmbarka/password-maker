75e4f80938c5c23e5afbc40090e82ba44a6e7823
Marouen Barka <barka.marouen@gmail.com>
2021-09-17 17:54:03 +0100

    projet generateur de password v1

---
+++ script.js
@@ -0,0 +1,36 @@
+const dataLowercase ="azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = "0123456789";
+const dataSymbols = "^$ù*,;:!?./§%µ¨£&\"'(-_@)";
+
+const data = [];
+const passwordLength= document.getElementById("display-password-length");
+const passwordOutput= document.getElementById("password-output");
+let password = '';
+
+function generatePassword()
+{
+    if(lowercase.checked) data.push(...dataLowercase);
+    if(uppercase.checked) data.push(...dataUppercase);
+    if(numbers.checked) data.push(...dataNumbers);
+    if(symbols.checked) data.push(...dataSymbols);
+    
+    for(i=0; i<passwordLength.value ; i++)
+    {
+        password += data[Math.floor(Math.random()*data.length)];
+    }
+
+    passwordOutput.value = password;
+
+    passwordOutput.select();
+
+    generateButton.textContent = 'Copied !';
+    document.execCommand("copy");
+
+    setTimeout(() =>{generateButton.textContent = 'Génerer un mot de passe'
+        },
+    2000);
+    
+}
+
+generateButton.addEventListener('click', generatePassword);
